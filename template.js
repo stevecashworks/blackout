0648eaaabe94f150039225f46152154af96ec18d
stevecashworks <84691613+stevecashworks@users.noreply.github.com>
2024-10-03 04:09:00 -0700

    Update template.js

--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-const createMessageWithTemplate=(code,name)=>{
+const createMessageWithTemplate=(code,name,link)=>{
 
     return`
 <!DOCTYPE html>
@@ -77,7 +77,7 @@ Click the link below to stay updated!
 </p>
 
       
-      <a href="https://chat.whatsapp.com/HExd89s4mkMATEdInpnQbU" class="button" style="color:white;">Join the party</a>
+      <a href=`${link}` class="button" style="color:white;">Join the party</a>
     </div>
     <div class="email-footer">
       <p class="black">&copy; 2024 Team Blackout. All rights reserved.</p>

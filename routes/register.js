8548f827c3016fb3853c9cda77f502026952e70f
stevecashworks <84691613+stevecashworks@users.noreply.github.com>
2024-10-03 04:04:06 -0700

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -5,12 +5,13 @@ import sendMail from "../sendMail.js"
 
 const register= async(req,res,next)=>{
 const plan= req.body.package
+const link= plan==="exlusive"?"https://chat.whatsapp.com/BgbY0xZFdx97BFZobIlgfZ":"https://chat.whatsapp.com/I44ChddVgqsLKtGv9wCf4F"
     try {
     const newUser= await userModel.create(req.body)
     const number= await generateTicket()
     const  newTicket= await ticketModel.create({number,user:newUser._id, plan})
     const updatedUser= await userModel.findByIdAndUpdate(newUser._id,{$set:{ticket:newTicket._id}})
-    sendMail(newUser.email,number,newUser.userName )
+    sendMail(newUser.email,number,newUser.userName,link )
     return res.status(200).json({
         success:true,
         result:`Successful`
